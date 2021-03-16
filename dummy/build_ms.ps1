ca0518673318fab22d1366d9cf41e0492d1d9959
Bassem Ramzy <SanPatBRS@outlook.com>
2021-03-16 15:21:46 +0200

    Add MSBuild PowerShell Script for Issue [Setup dummy library to compile as github action #3]

---
+++ dummy/build_ms.ps1
@@ -0,0 +1,119 @@
+# Get MS Build Path
+$Script_Path = Get-Location
+$Babel_Path = (get-item $Script_Path).parent.FullName
+
+Write-Host "Checking MS Visual Studio installation folder on C:\"
+$C_Path = “C:\”
+if(!(Test-Path -Path $C_Path))
+{
+    Write-Host -ForegroundColor RED "C:\ Is Not Found"
+    exit
+}
+$MSVS_Installer_path = "C:\Program Files (x86)\Microsoft Visual Studio\Installer"
+if(!(Test-Path -Path $MSVS_Installer_path))
+{
+    Write-Host -ForegroundColor RED "Visual Studio Installaer is not found"
+    exit
+}
+else
+{
+    Write-Host -ForegroundColor GREEN "Found MS Visual Studio installation: " $MSVS_Installer_path
+    cd $MSVS_Installer_path
+}
+$MSVS_Installation_Path = .\vswhere.exe -property InstallationPath
+Write-Host -ForegroundColor GREEN "Using MSVS Installation On: " $MSVS_Installation_Path
+
+Write-Host "Checking MSBuild Installation"
+$MSBuild_Installation_Path = $MSVS_Installation_Path + "\MSBuild\Current\Bin"
+if(!(Test-Path -Path $MSBuild_Installation_Path))
+{
+    Write-Host -ForegroundColor RED "MSBuild is not found, please install it"
+    exit
+}
+else
+{
+    Write-Host -ForegroundColor GREEN "Found MSBuild Installation: " $MSBuild_Installation_Path
+    cd $MSBuild_Installation_Path
+}
+
+# _____________
+# Build x86 DLL
+#
+Write-Host ""
+Write-Host -BackgroundColor GREEN "Building x86 DLL          "  
+
+[String]$x86_Release_Path = [String]$Script_Path + "\msvscpp\msvscpp\Release"
+if(!(Test-Path -Path $x86_Release_Path))
+{
+    mkdir $x86_Release_Path | out-null
+}
+
+[String]$Log_File_Path =
+    [String]$Script_Path + "\msvscpp\msvscpp\Release\Build_Output_" + [DateTime]::Now.ToString("yyyyMMdd-HHmmss") + ".log"
+
+[String]$MSBuild_Cmd = 
+    (".\MSBuild ") + 
+    ([String]$Script_Path + "\msvscpp\msvscpp\msvscpp.vcxproj" + " /property:Configuration=Release /property:Platform=x86") + 
+    ("  >  " + [String]$Log_File_Path)
+
+Invoke-expression $MSBuild_Cmd
+Write-Host "Build is done, log file is found in: "$Log_File_Path
+
+Write-Host "Checking build results"
+#Select-String -Path $Log_File_Path -Pattern 'Error'
+
+[String]$x86_DLL_Path = [String]$x86_Release_Path + "\xHPTDC8_dummy.dll"
+[String]$x86_Lib_Path = [String]$x86_Release_Path + "\xHPTDC8_dummy.lib"
+if(!(Test-Path -Path $x86_DLL_Path))
+{
+    Write-Host -ForegroundColor RED "Error generating DLL"
+}
+else
+{
+    [String]$Babel_Libx86_Path = $Babel_Path + "\lib\x86"
+    Copy-Item $x86_DLL_Path -Destination $Babel_Libx86_Path
+    Copy-Item $x86_Lib_Path -Destination $Babel_Libx86_Path
+    Write-Host -ForegroundColor GREEN "DLL and Lib are copied successfully to the corresponding lib folder: " $Babel_Libx86_Path
+}
+
+
+# _____________
+# Build x64 DLL
+#
+Write-Host ""
+Write-Host -BackgroundColor GREEN "Building x64 DLL          " 
+
+[String]$x64_Path = [String]$Script_Path + "\msvscpp\msvscpp\x64"
+if(!(Test-Path -Path $x64_Path))
+{
+    mkdir $x64_Path | out-null
+    [String]$x64_Release_Path = [String]$Script_Path + "\msvscpp\msvscpp\x64\Release"
+    mkdir $x64_Release_Path | out-null
+}
+
+[String]$Log_File_Path =
+    [String]$Script_Path + "\msvscpp\msvscpp\x64\Release\Build_Output_" + [DateTime]::Now.ToString("yyyyMMdd-HHmmss") + ".log"
+
+[String]$MSBuild_Cmd = 
+    (".\MSBuild ") + 
+    ([String]$Script_Path + "\msvscpp\msvscpp\msvscpp.vcxproj" + " /property:Configuration=Release /property:Platform=x64") + 
+    ("  >  " + [String]$Log_File_Path)
+
+Invoke-expression $MSBuild_Cmd
+Write-Host "Build Log Is Found In: "$Log_File_Path
+
+Write-Host "Checking Build Results"
+
+[String]$x64_DLL_Path = [String]$x64_Release_Path + "\xHPTDC8_dummy.dll"
+[String]$x64_Lib_Path = [String]$x64_Release_Path + "\xHPTDC8_dummy.lib"
+if(!(Test-Path -Path $x64_DLL_Path))
+{
+    Write-Host -ForegroundColor RED "Error generating DLL"
+}
+else
+{
+    [String]$Babel_Libx64_Path = $Babel_Path + "\lib\x64"
+    Copy-Item $x64_DLL_Path -Destination $Babel_Libx64_Path
+    Copy-Item $x64_Lib_Path -Destination $Babel_Libx64_Path
+    Write-Host -ForegroundColor GREEN "DLL and Lib are copied successfully to the corresponding lib folder: " $Babel_Libx64_Path
+}
