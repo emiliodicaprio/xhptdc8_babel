a6856018b6dddae50f6063db22e8d8152b1e6b4f
Bassem-Ramzy <75851720+Bassem-Ramzy@users.noreply.github.com>
2021-03-31 09:13:23 +0200

    Paths and Sample new function

---
+++ util/util_unit_test/NewFunction.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace newFunction	// Test Cases of a new util function, rename the file and namespace to API name 
+{
+	TEST_CLASS(happy_scenario)
+	{
+	public:
+		// Placeholder to new test case
+		TEST_METHOD(case1)
+		{
+			Assert::AreEqual(1, 1);	
+		}
+	};
+};
